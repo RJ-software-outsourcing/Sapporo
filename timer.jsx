7d90a7a8b35326588c853380fe45f55fcc62b8f8
Chen, Wei-Chao <willy.chen@hp.com>
2015-11-13 15:45:49 +0800

    Update Meteor components (except React)
    - Keep React unchanged because newer version causes onClick event not
    firing
    - Annoying log message due to conflict between React and MDL is gone
    after this update

--- timer.jsx
+++ timer.jsx
@@ -15,7 +15,6 @@ Timer = React.createClass({
     }
   },
   render() {
-    // Just render a placeholder container that will be filled in
     return (
       <div className="Timer">
         {(this.displayMessage()).coding?

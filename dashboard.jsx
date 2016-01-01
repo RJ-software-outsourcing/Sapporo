bd2781ce18f209ff222545da3df2a79ff56e8f40
Chenwil <catsass19@hotmail.com>
2016-01-02 03:41:08 +0800

    basic dashboard items
    - total score
    - pass count

--- dashboard.jsx
+++ dashboard.jsx
@@ -23,21 +23,59 @@ DashboardInstance = React.createClass({
     mixins: [ReactMeteorData],
         getMeteorData () {
             return {
-                userData: userDataCollection.findOne({username: Meteor.user()})
+                userData: userDataCollection.findOne({username: Meteor.user().username}),
+                problems: Problems.find({}).fetch()
             }
     },
     totalScore () {
-        
+        var score = 0;
+        if (!this.data.userData || !this.data.userData.hasOwnProperty('pass')) {
+            return '';
+        }
+        this.data.userData.pass.forEach(function (item) {
+            var problem = Problems.findOne({_id:item});
+            if (problem) {
+                var tmp = Number(problem.score);
+                if (!isNaN(tmp)) {
+                    score += tmp;
+                }
+            }
+        });
         return (
-            <div>
-
+            <div className="mdl-cell mdl-cell--4-col">
+                <span>Total Score</span>
+                <h3>{score}</h3>
+            </div>
+        );
+    },
+    problemPass () {
+        var count = 0;
+        if (!this.data.userData || !this.data.userData.hasOwnProperty('pass')) {
+            return '';
+        }
+        this.data.userData.pass.forEach(function (item) {
+            var problem = Problems.findOne({_id:item});
+            if (problem) {
+                count += 1;
+            }
+        });
+        return (
+            <div className="mdl-cell mdl-cell--4-col">
+                <span>Problem Pass</span>
+                <h3>{count} / {this.data.problems.length}</h3>
             </div>
         );
     },
     render() {
         return (
-            <div style={{border:'1px solid red'}}>
-                <span>{this.totalScore()}</span>
+            <div>
+                <div className="dashboardGrid mdl-grid" style={{border:'1px solid red'}}>
+                    {this.totalScore()}
+                    {this.problemPass()}
+                    <div className="mdl-cell mdl-cell--4-col">
+                        Total Score:
+                    </div>
+                </div>
             </div>
         );
     }

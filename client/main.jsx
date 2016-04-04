2d10ea306a6389f966c49641051f419d477db1bb
Chenwil <catsass19@hotmail.com>
2016-04-04 13:54:28 +0800

    Basic structure - Header, Menu and Timer

--- client/main.jsx
+++ client/main.jsx
@@ -3,28 +3,53 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import App from 'grommet/components/App';
 import Header from 'grommet/components/Header';
-import Footer from 'grommet/components/Footer';
 import Title from 'grommet/components/Title';
+import Box from 'grommet/components/Box'
+import Menu from 'grommet/components/Menu'
+import Anchor from 'grommet/components/Anchor'
+import MenuIcon from 'grommet/components/icons/base/Menu'
+
+import Timer from '../imports/ui/Timer.jsx';
+
+
+class MainMenu extends Component {
+    render () {
+        return (
+            <Menu icon={<MenuIcon />}>
+                <Anchor href="#">
+                    First
+                </Anchor>
+                <Anchor href="#">
+                    Second
+                </Anchor>
+                <Anchor href="#">
+                    Third
+                </Anchor>
+            </Menu>
+        );
+    }
+};
+
 
 class Main extends Component {
   render () {
     return (
-      <App centered={false}>
-        <Header direction="row" justify="between" large={true}
-          pad={{horizontal: 'medium'}}>
-          <Title>Test</Title>
-        </Header>
-        <Footer primary={true} appCentered={true} direction="column"
-          align="center" pad="small" colorIndex="grey-1">
-          <p>
-            Build your ideas with <a href="http://grommet.io" target="_blank">Grommet</a>!
-          </p>
-        </Footer>
-      </App>
+        <App>
+            <Header direction="row" large={true} justify="between"
+                    pad={{horizontal: 'medium'}} colorIndex="neutral-1">
+                <MainMenu/>
+                <Box direction="row" pad={{between: 'medium'}}>
+                    <Timer />
+                </Box>
+            </Header>
+        </App>
     );
   }
 };
+
+
+
 Meteor.startup(() => {
-    let element = document.getElementById('content');
+    let element = document.getElementById('main');
     render(React.createElement(Main), element);
 });
---
+++ imports/ui/Timer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Box from 'grommet/components/Box'
+
+export default class Timer extends Component {
+  render () {
+    return (
+        <Box>Timer</Box>
+    );
+  }
+};

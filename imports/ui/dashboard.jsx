4c4dbb050fcf1d3fcc029b80821d2b63e37b00d2
Chenwil <willy.chen@hpe.com>
2016-04-17 22:52:13 +0800

    Play with color of tiles a little bit

--- imports/ui/dashboard.jsx
+++ imports/ui/dashboard.jsx
@@ -23,17 +23,18 @@ const cellHeight = function () {
         return 200;
     }
 };
-
+const customTileStyle = {
+    height: 'inherit',
+    textAlign:'center',
+    lineHeight: String(cellHeight())+'px',
+    fontSize: String(cellHeight())+'%',
+    fontWeight: 'bold',
+    color: 'white'
+};
 const getTimerTile = function (timer) {
-    const timeStyle = {
-        height: 'inherit',
-        textAlign:'center',
-        lineHeight: String(cellHeight())+'px',
-        fontSize: String(cellHeight())+'%',
-        fontWeight: 'bold'
-    };
+
     return(
-        <div style={timeStyle}>
+        <div style={customTileStyle}>
             {timer}
         </div>
     );
@@ -51,36 +52,22 @@ class Dashboard extends Component {
     }
     getScoreTile () {
         if (!this.props._problem || !Meteor.user()) return;
-        const scoreStyle = {
-            height: 'inherit',
-            textAlign:'center',
-            lineHeight: String(cellHeight())+'px',
-            fontSize: String(cellHeight())+'%',
-            fontWeight: 'bold'
-        };
         let userData = getCurrentUserData(Meteor.user()._id, this.props._userData);
         let score = getUserTotalScore(userData, this.props._problem);
         let totalScore = getTotalScore(this.props._problem);
         return (
-            <div style={scoreStyle}>
+            <div style={customTileStyle}>
                 <span>{score} / {totalScore}</span>
             </div>
         );
     }
     getPassProblemTile () {
         if (!this.props._problem || !Meteor.user()) return;
-        const passStyle = {
-            height: 'inherit',
-            textAlign:'center',
-            lineHeight: String(cellHeight())+'px',
-            fontSize: String(cellHeight())+'%',
-            fontWeight: 'bold'
-        };
         let totalProblem = this.props._problem.length;
         let userData = getCurrentUserData(Meteor.user()._id, this.props._userData);
-        let passProblem = getUserPassedProblem(userData, this.props._problem)
+        let passProblem = getUserPassedProblem(userData, this.props._problem);
         return (
-            <div style={passStyle}>
+            <div style={customTileStyle}>
                 <span>{passProblem} / {totalProblem}</span>
             </div>
         );
@@ -88,7 +75,7 @@ class Dashboard extends Component {
     getContent (tile) {
         let contentStyle = {
             height: 'inherit',
-            backgroundColor: tile.backgroundColor? tile.backgroundColor:'rgba(255,255,255,0.4)'
+            backgroundColor: tile.backgroundColor? tile.backgroundColor:'rgba(255,255,255,0.3)'
         };
         return (
             <div style={contentStyle}>
@@ -101,39 +88,43 @@ class Dashboard extends Component {
             title: 'Hi ' + Meteor.user().username,
             featured: true,
             cols: 2,
-            image: '/images/1.jpg'
+            image: '/images/1.jpg',
+            backgroundColor: 'rgba(0,0,80,0.5)',
         }, {
             title: 'Live Feed Messages',
             cols: 2,
-            image: '/images/6.jpg'
+            image: '/images/6.jpg',
+            backgroundColor: 'rgba(0,80,0,0.5)'
         }, {
             title: 'Time',
             cols: 2,
             image: '/images/3.jpg',
-            backgroundColor: 'rgba(255,255,255,0.8)',
+            backgroundColor: 'rgba(80,0,0,0.5)',
             content: getTimerTile(<Timer/>)
         }, {
             title: 'Total Score',
             featured: true,
             cols: 1.5,
             image: '/images/4.jpg',
-            backgroundColor: 'rgba(255,255,255,0.8)',
+            backgroundColor: 'rgba(40,0,40,0.5)',
             content: this.getScoreTile()
         }, {
             title: 'Passed Problems',
             cols: 1.5,
             image: '/images/5.jpg',
-            backgroundColor: 'rgba(255,255,255,0.8)',
+            backgroundColor: 'rgba(0,50,50,0.5)',
             content: this.getPassProblemTile()
         }, {
             title: 'Online Help',
             featured: true,
             cols: 1.5,
-            image: '/images/3.jpg'
+            image: '/images/3.jpg',
+            backgroundColor: 'rgba(80,80,0,0.5)'
         }, {
             title: 'About Us',
             cols: 1.5,
-            image: '/images/5.jpg'
+            image: '/images/5.jpg',
+            backgroundColor: 'rgba(80,80,160,0.5)'
         }];
         return (
             <div>

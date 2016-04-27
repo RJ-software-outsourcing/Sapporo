79cf82d25e6c478b1ab612103223286efb354596
Chenwil <willy.chen@hpe.com>
2016-04-27 17:56:03 +0800

    - Add yml file for TravisCI - Add ruby and Swift in Program Family

--- imports/library/lang_import.js
+++ imports/library/lang_import.js
@@ -2,6 +2,7 @@ import * as java       from 'brace/mode/java';
 import * as javascript from 'brace/mode/javascript';
 import * as c_cpp      from 'brace/mode/c_cpp';
 import * as python     from 'brace/mode/python';
+import * as ruby       from 'brace/mode/ruby';
+import * as swift      from 'brace/mode/swift';
 
-
-export {java, javascript, c_cpp, python};
+export {javascript, c_cpp, python, java, ruby, swift};

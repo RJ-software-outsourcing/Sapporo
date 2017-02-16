7437d81dfb050a98d9acc34d4a67500943ee0f8a
kenny <kenny@localhost.localdomain>
2017-02-16 03:52:08 -0500

    #20 support options that are user number and delay time.
    User can create sapporo users number they want and set request delay time.
    
    for shell script type
    simu_helper.sh $numberOfUsers $delaytime $url $lantype
    
    for meteor-down
    meteor-down simu_sapporo_user.js $saporro_user $url $lantype [$delaytime]
    default delay time is 3 sec.

--- .auto_test/simu_helper.sh
+++ .auto_test/simu_helper.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
 
-url=$1
-lantype=$2
+stop=0
 
-saporro_users='aaa bbb ccc ddd eee fff ggg'
+if [[ $1 == "stop" ]]; then
+  echo stop
+  `ps -Af | grep simu_sapporo_user.js | awk '{print $2}' | xargs kill`
+  ps -Af | grep simu_sapporo_user.js | awk '{print $2}'
+  exit 0
+fi
+
+
+users=$1
+delaytime=$2
+url=$3
+lantype=$4
+cnt=0
+
+saporro_users=()
+
+while [ $cnt -lt $users ]
+do
+  user=`echo aaa$cnt`
+  saporro_users=`echo "$saporro_users $user"`
+  cnt=$(($cnt+1))
+done
 
 for saporro_user in $saporro_users; do
-  sapporo_pid=`meteor-down simu_sapporo_user.js  $saporro_user $url $lantype > $saporro_user &`
+  sapporo_pid=`meteor-down simu_sapporo_user.js  $saporro_user $url $lantype $delaytime > $saporro_user &`
   pids+=($sapporo_pid)
 done 
 
--- .auto_test/simu_sapporo_user.js
+++ .auto_test/simu_sapporo_user.js
@@ -1,9 +1,17 @@
-console.log(process.argv)
-
 var user = process.argv[3]
 var url = process.argv[4]  // example "http://127.0.0.0:3000"
 var lantype = process.argv[5]
+var delaytime = process.argv[6]
 
+if (!delaytime) {
+  delaytime=3000
+}
+
+
+console.log(user)
+console.log(url)
+console.log(lantype)
+console.log(delaytime)
 
 meteorDown.init(function (Meteor) {
   console.log( user +  " submit ***********")
@@ -15,15 +23,21 @@ meteorDown.init(function (Meteor) {
        langType: lantype
     }, 
     function (error, result) {
-      console.log(arguments);
-      console.log(error);
-      console.log(result);
 
+      if(!error){
+        console.log("\trequest succeful");
+      }else{
+        console.log("\t" + error)
+      }
+      console.log("\t"+result);
+
+      console.log(new Date());
       
       setTimeout(function(){
+        console.log(new Date());
         console.log("User leave")
         Meteor.kill();
-      }, 10000);
+      }, delaytime);
     });
 });
 

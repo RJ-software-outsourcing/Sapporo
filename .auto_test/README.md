9e15672e8c42e80ba4493bddec6230f774a87103
kenny <kenny@localhost.localdomain>
2017-04-11 05:27:30 -0400

    Support multiple languages and compile error testing
    Implement a argument parser. User can use -lc -lp2 -lj to set language IDs.
    Support C, C compile error, python2.
    Modify helper.sh to adapt to new javascript arguments.
    
    Add log function in javascript file.
    Add -h option to print help.

--- .auto_test/README.md
+++ .auto_test/README.md
@@ -1,8 +1,14 @@
-There will be no logs if there is no any Error
-All output redirects to dev/null
+simu_sapporo_user.js is a script which is based on meteor-down to simulate user using Sapporo.
 
+For multiple users, you can use simu_helper.sh to create them at once.
+Each user's log will be in aaa<user number>
 
+Lang ID is a ID in Sapporo DB for program language item.
 cmd Example
-*                    simu_helper.sh \<user number\> \<url\> \<lanTpye\> \<time\>
-*  meteor-down simu_sapporo_user.js \<user name\>   \<url\> \<lanTpye\> \<time\>
+*                    simu_helper.sh \<user number\> \<url\> \<time\> \<c lang ID\> \<python 2 lang ID\>
+*                    simu_helper.sh stop // Stop All meteor.
+*   meteor-down simu_sapporo_user.js <user> <url> [-t <delay time> -lc <c ID> -lp2 <python2 ID> -lj <java ID>]
+    Ex:
+      meteor-down simu_sapporo_user.js aaa http://localhost:4000 -t 3000 -lc KELLL581KSCxxPP -lp2 iK59adkaokwa -lj sefjislIKJLLL
+
 
--- .auto_test/simu_sapporo_user.js
+++ .auto_test/simu_sapporo_user.js
@@ -1,7 +1,92 @@
-var user = process.argv[3]
-var url = process.argv[4]  // example "http://127.0.0.0:3000"
-var lantype = process.argv[5]
-var delaytime = process.argv[6]
+
+var testQuestions = {
+  p2:{
+    name: "python2 success",
+    lang: "",
+    code: 'print "submit test"'      
+  },
+  c:{
+    name: "C success",
+    lang: "",
+    code: '#include<stdio.h>\n void main(){printf("submit test C");}' 
+  },
+  cError:{
+    name: "C compile Error",
+    lang: "",
+    code: '#include<stdio.h>\n void main(){printf("submit test C");}' 
+  }
+};
+
+var codeCollection=[];
+
+
+var user = ""
+var url = ""  // example "http://127.0.0.0:3000"
+var delaytime = ""
+
+var user = "error.log"
+
+if (process.argv.length < 5){
+
+  if ("-h" == process.argv[3]){
+    console.log("Usage: ");
+    console.log("  meteor-down simu_sapporo_user.js <user> <url> [-t <delay time> -lc <c ID> -lp2 <python2 ID> -lj <java ID>]");
+    console.log("  Ex:");
+    console.log("    meteor-down simu_sapporo_user.js aaa http://localhost:4000 -t 3000 -lc KELLL581KSCxxPP -lp2 iK59adkaokwa -lj sefjislIKJLLL");
+    process.exit(0)
+  }
+}
+
+if (process.argv.length < 7){
+  logMessage("too few argument")    
+  process.exit(1); 
+}
+
+process.argv.forEach(function(ele, idx){
+
+  var ltype = ""
+
+  if( 3 == idx ){
+    user =  process.argv[idx] 
+  }else if(4 == idx){
+    url = process.argv[idx]
+  }else if( 4 < idx){
+     // Parse option -t -lc -lcpp -lp2 -lp3 -lj
+    if ("-t" == ele){
+      delaytime = process.argv[idx + 1];
+    }else if ("-lc" == ele){
+      ltype = process.argv[idx + 1];
+      testQuestions.c.lang = ltype;
+      codeCollection.push(testQuestions.c);
+      testQuestions.cError.lang = ltype;
+      codeCollection.push(testQuestions.cError);
+    }else if ("-lcpp" == ele){
+
+      console.log("cpp ID: " + process.argv[idx + 1])
+
+    }else if ("-lp2" == ele){
+      ltype = process.argv[idx + 1];
+      testQuestions.p2.lang = ltype;
+      codeCollection.push(testQuestions.p2);
+
+    }else if ("-lp3" == ele){
+      console.log("python 3 ID: " + process.argv[idx + 1])
+
+    }else if ("-lj" == ele){
+      console.log("java ID: " + process.argv[idx + 1])
+    }
+  }
+});
+
+var maxQuest = codeCollection.length;
+
+// To do: keep log message in arrage and output every 10 reocrds.
+// If user interrupt it. Write all log to file and exit.
+process.on('SIGINT', () =>{
+  console.log('Received SIGINT.');
+  process.exit();
+})
+
 
 if (!delaytime) {
   delaytime=3000
@@ -9,24 +94,51 @@ if (!delaytime) {
 
 function logMessage(msg){
   var fs = require('fs');
-  fs.appendFileSync( "./" + user, msg, 'utf8'  ); 
+  fs.appendFileSync( "./" + user, msg + '\n', 'utf8'  ); 
+}
+
+
+var startTime=0
+function StartMesureTime(){
+  startTime = (new Date).getTime();
+}
+
+function EndMesureTime(){
+  var diffms = (new Date).getTime() - startTime;
+  var diffSec = diffms/1000
+
+  logMessage( diffms + "ms"  );
+}
+
+function GetATestCode(){
+  var idx = Math.floor(Math.random() * maxQuest);
+  console.log(idx);
+  return codeCollection[idx];
 }
 
 meteorDown.init(function (Meteor) {
 
+  StartMesureTime();
+  var testCode = GetATestCode();
+  logMessage(testCode.name + "  ");
+
   Meteor.call('docker.performanceTest',
     { 
-       code: 'print "submit by' + user +  '"',
+       code: testCode.code,
        input: 'hahaha',
-       langType: lantype
+       langType: testCode.lang
     }, 
     function (error, result) {
 
+      EndMesureTime();
+
+      var endTime = new Date();
+      console.log(result);
+
       if(error){
-        logMessage("Error with Meteor\n");
-      }
-      
-      if(  result.indexOf("submit") < 0 ){
+        logMessage("Error with Meteor\n" + result);
+      }      
+      else if(  result.indexOf("submit") < 0 ){
         logMessage("Error " + result);
       }     
 
